f27e06a9750b4c785e9bcafd0b84b75799b53af0
Zach Knight <aeros.storkpk@gmail.com>
2019-08-16 09:39:34 -0500

    String helper for packing chat messages

--- pkg/strutil/conversion.go
+++ pkg/strutil/conversion.go
@@ -1,19 +1,68 @@
 package strutil
 
 import (
+	"fmt"
+	"strings"
 	"unicode"
 )
 
+func init() {
+	fmt.Printf("msg: %v, %v\n", PackChatMessage("lol cool story"), UnpackChatMessage(PackChatMessage("lol cool story")))
+}
+
 // Presumably this charset is optimized to be in order of most-used in the English language.
 // Not sure how Jagex came up with this arrangement, along with their interesting bit-packing methods involved here
-var charset = []byte{' ', 'e', 't', 'a', 'o', 'i', 'h', 'n', 's', 'r', 'd', 'l', 'u', 'm', 'w', 'c', 'y', 'f', 'g', 'p', 'b', 'v', 'k', 'x', 'j', 'q', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '!', '?', '.', ',', ':', ';', '(', ')', '-', '&', '*', '\\', '\'', '@', '#', '+', '=', '\243', '$', '%', '"', '[', ']'}
+var charset = []rune{' ', 'e', 't', 'a', 'o', 'i', 'h', 'n', 's', 'r', 'd', 'l', 'u', 'm', 'w', 'c', 'y', 'f', 'g', 'p', 'b', 'v', 'k', 'x', 'j', 'q', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ', '!', '?', '.', ',', ':', ';', '(', ')', '-', '&', '*', '\\', '\'', '@', '#', '+', '=', '\243', '$', '%', '"', '[', ']'}
+
+func getCharCode(c rune) int {
+	for i, cs := range charset {
+		if cs == c {
+			return i
+		}
+	}
+
+	return 0
+}
+
+func PackChatMessage(msg string) []byte {
+	var buf []byte
+	if len(msg) > 80 {
+		msg = msg[:80]
+	}
+	msg = strings.ToLower(msg)
+	cachedValue := -1
+	for _, c := range msg {
+		code := getCharCode(c)
+		if code > 12 {
+			code += 195
+		}
+		if cachedValue == -1 {
+			if code < 13 {
+				cachedValue = code
+			} else {
+				buf = append(buf, byte(code))
+			}
+		} else if code < 13 {
+			buf = append(buf, byte((cachedValue<<4)+code))
+			cachedValue = -1
+		} else {
+			buf = append(buf, byte((cachedValue<<4)+(code>>4)))
+			cachedValue = code & 0xF // Bottom 4 bits
+		}
+	}
+	if cachedValue != -1 {
+		buf = append(buf, byte(cachedValue<<4))
+	}
+
+	return buf
+}
 
 //UnpackChatMessage Takes a byte slice as input, and decodes it into a human readable chat message.
 //  Works by unpacking the bits into 2 4-bit values.  86% of the time, this is enough information for the output rune.
 //  14% of the time, it will cache this value and use the next 4 bits to help decode the rune.
 //  The first 13 runes in the charset map 1 to 1 for 13/15 possibilities within each 4 bits.
 func UnpackChatMessage(data []byte) string {
-	var buf []byte
+	var buf []rune
 	dataOffset := 0
 	cachedValue := -1
 	for i1 := 0; i1 < len(data); i1++ {
