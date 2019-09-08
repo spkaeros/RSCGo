5b866e0c754076b7368709d6a4902158996bb49e
Zach Knight <aeros.storkpk@gmail.com>
2019-09-08 05:15:51 -0500

    Add close gate handler; fixme:seems to not be 100% right

--- pkg/server/scene_interactions.go
+++ pkg/server/scene_interactions.go
@@ -5,10 +5,13 @@ import (
 	"bitbucket.org/zlacki/rscgo/pkg/server/packets"
 )
 
-var ObjectCommands = make(map[string]func(p *entity.Player, o *entity.Object))
+var objectCommandHandlers = make(map[string]func(p *entity.Player, o *entity.Object))
+var objectCommand2Handlers = make(map[string]func(p *entity.Player, o *entity.Object))
+var objectHandlers = make(map[int]func(p *entity.Player, o *entity.Object))
+var object2Handlers = make(map[int]func(p *entity.Player, o *entity.Object))
 
 func init() {
-	ObjectCommands["open"] = func(p *entity.Player, o *entity.Object) {
+	objectHandlers[57] = func(p *entity.Player, o *entity.Object) {
 		p.DistancedAction = func() {
 			if p.WithinRange(*o.Location(), 1) {
 				newDoor := entity.NewObject(58, o.Direction, o.X(), o.Y(), false)
@@ -19,10 +22,33 @@ func init() {
 			}
 		}
 	}
+	object2Handlers[58] = func(p *entity.Player, o *entity.Object) {
+		p.DistancedAction = func() {
+			if p.WithinRange(*o.Location(), 1) {
+				newDoor := entity.NewObject(57, o.Direction, o.X(), o.Y(), false)
+				region := entity.GetRegionFromLocation(*o.Location())
+				region.Objects.RemoveObject(o)
+				region.Objects.AddObject(newDoor)
+				p.ResetDistancedAction()
+			}
+		}
+	}
 	PacketHandlers["objectaction"] = func(c *Client, p *packets.Packet) {
 		targetObject := c.player.LocalObjects.GetObject(p.ReadShort(), p.ReadShort())
 		if targetObject != nil {
-			if handler, ok := ObjectCommands[ObjectDefinitions[targetObject.ID].Commands[0]]; ok {
+			if handler, ok := objectHandlers[targetObject.ID]; ok {
+				handler(c.player, targetObject)
+			} else if handler, ok := objectCommandHandlers[ObjectDefinitions[targetObject.ID].Commands[0]]; ok {
+				handler(c.player, targetObject)
+			}
+		}
+	}
+	PacketHandlers["objectaction2"] = func(c *Client, p *packets.Packet) {
+		targetObject := c.player.LocalObjects.GetObject(p.ReadShort(), p.ReadShort())
+		if targetObject != nil {
+			if handler, ok := object2Handlers[targetObject.ID]; ok {
+				handler(c.player, targetObject)
+			} else if handler, ok := objectCommand2Handlers[ObjectDefinitions[targetObject.ID].Commands[1]]; ok {
 				handler(c.player, targetObject)
 			}
 		}
