e6faea94888d41113a91fcec3cca89953ef5a86e
Zachariah Knight <aeros.storkpk@gmail.com>
2019-10-22 10:08:43 -0500

    Remove unused old list pkg

--- pkg/list/doc.go
+++
@@ -1,8 +0,0 @@
-package list
-
-/**
- * Package list implements a generic slice-backed list API that can hold any types, and the types don't even have
- * to be uniform, though in practice they probably should be.  I may phase out this code in the near future,
- * as honestly it is nothing more than a wrapper around a standard go slice.  I am not sure, but I believe it is
- * VERY similar to the container/list package in the Go standard library.
- */
