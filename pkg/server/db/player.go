a95d15a50dd47e0e579956be8b249deb93a880ac
Zachariah Knight <aeros.storkpk@gmail.com>
2019-11-27 15:30:17 -0600

    Fix yet another bug with player appearances

--- pkg/server/db/player.go
+++ pkg/server/db/player.go
@@ -94,6 +94,8 @@ func LoadPlayerProfile(usernameHash uint64, password string, loginReply chan byt
 	rows.Scan(&player.DatabaseIndex, &x, &y, &player.Rank, &player.Appearance.HeadColor, &player.Appearance.BodyColor, &player.Appearance.LegsColor, &player.Appearance.SkinColor, &player.Appearance.Head, &player.Appearance.Body)
 	//	player.Location = world.NewLocation(x, y)
 	//	player.Teleport(220, 445)
+	player.Equips[0] = player.Appearance.Head
+	player.Equips[1] = player.Appearance.Body
 	player.SetX(x)
 	player.SetY(y)
 	return nil
