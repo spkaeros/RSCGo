e414d0fd427274062850293783de828105655f1e
Zachariah Knight <aeros.storkpk@gmail.com>
2020-02-02 22:30:55 -0600

    fix bugs from code refactoring

--- pkg/server/client.go
+++ pkg/server/client.go
@@ -81,6 +81,7 @@ func (c *client) startNetworking() {
 	}()
 	go func() {
 		defer c.destroy()
+		defer close(incomingPackets)
 		defer awaitDeath.Wait()
 		defer c.player.Destroy()
 		for {
@@ -101,20 +102,20 @@ func (c *client) startNetworking() {
 //destroy Safely tears down a client, saves it to the database, and removes it from server-wide player list.
 func (c *client) destroy() {
 	c.destroyer.Do(func() {
-		log.Info.Printf("Unregistered: %v\n", c.player.String())
 		//close(c.IncomingPackets)
 		close(c.player.OutgoingPackets)
 		if err := c.socket.Close(); err != nil {
 			log.Error.Println("Couldn't close socket:", err)
 		}
+		if player, ok := world.Players.FromIndex(c.player.Index); ok || player != c.player {
+			log.Warning.Println("Destroying Player did not match player that is assigned index in map!")
+			return
+		}
+		log.Info.Printf("Unregistered: %v\n", c.player.String())
 		world.RemovePlayer(c.player)
 		c.player.SetRegionRemoved()
 		c.player.SetConnected(false)
 		world.Players.BroadcastLogin(c.player, false)
-		if player, ok := world.Players.FromIndex(c.player.Index); ok && player != c.player {
-			log.Warning.Println("Destroying Player did not match player that is assigned index in map!")
-			return
-		}
 		go db.SavePlayer(c.player)
 		world.Players.Remove(c.player)
 	})
