03c286094fa8a844b6aff738a9d2db1f9b4d8732
Zachariah Knight <zach.knight@protonmail.com>
2020-03-31 10:26:50 -0400

    Fix bug where shop prices were inversely effected by the amount in the shop

--- pkg/game/world/shops.go
+++ pkg/game/world/shops.go
@@ -338,10 +338,10 @@ func (s *Shop) Clone() *Shop {
 // The formula simply subtracts the shop's base stocked amount of item from item's amount, and multiplies the difference
 // by ShopNormalDeltaRate.
 func (s *Shop) DeltaPercentMod(item *Item) int {
-	return s.Stock.Get(item.ID).DeltaAmount(item) * ShopNormalDeltaRate
+	return item.DeltaAmount(s.Stock.Get(item.ID)) * ShopNormalDeltaRate
 }
 
 func (s *Shop) DeltaPercentModID(id int) int {
-	return s.Stock.Get(id).DeltaAmount(s.Inventory.Get(id)) * ShopNormalDeltaRate
+	return s.Inventory.Get(id).DeltaAmount(s.Stock.Get(id)) * ShopNormalDeltaRate
 	//return (s.Stock.Count(id) - s.Inventory.Count(id)) * ShopNormalDeltaRate
 }
