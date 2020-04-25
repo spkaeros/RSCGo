154277d33ed846f460661c6894cdc247c491aea4
Zachariah Knight <zach.knight@protonmail.com>
2020-04-25 07:29:18 -0400

    Add soundfx on shop actions

--- pkg/game/net/handlers/shops.go
+++ pkg/game/net/handlers/shops.go
@@ -39,7 +39,6 @@ func init() {
 			}
 
 			if !player.Inventory.CanHold(id, 1) && player.Inventory.CountID(10) != price {
-				// If the player has exactly price count of coins, the item will take the slot of the coins
 				player.Message("You can't hold the objects you are trying to buy!")
 				return
 			}
@@ -49,15 +48,8 @@ func init() {
 			}
 			
 			player.AddItem(id, 1)
-			// TODO: Can't I simplify this to one API call?
-//			if shop.Stock.Count(id) == 0 && shop.Inventory.Count(id) == 1 {
-				// if the item is not regularly owned by the shop and there is no more
-//				shop.Inventory.RemoveID(id)
-//			} else {
-				// if the shop regularly owns the item
-//				shop.Inventory.Get(id).Amount--
-//			}
 			shop.Remove(id, 1)
+			player.PlaySound("coins")
 			shop.Players.RangePlayers(func(player *world.Player) bool {
 				player.SendPacket(world.ShopOpen(shop))
 				return false
@@ -76,8 +68,6 @@ func init() {
 			}
 
 			if !shop.Stock.Contains(id) && !shop.BuysUnstocked {
-				//log.Suspicious.Println(player, "tried buying item["+strconv.Itoa(id)+"] for ["+strconv.Itoa(price)+"gp] but the (" + shop.Name + ") shop is out of that item.")
-				// TODO: og rsc msg here
 				player.Message("This shop does not purchase foreign objects")
 				return
 			}
@@ -88,6 +78,7 @@ func init() {
 				return
 			}
 			if player.Inventory.RemoveByID(id, 1) > -1 {
+				player.PlaySound("coins")
 				player.AddItem(10, price)
 				shop.Inventory.Add(&world.Item{ID: id, Amount: 1})
 				shop.Players.RangePlayers(func(player *world.Player) bool {
